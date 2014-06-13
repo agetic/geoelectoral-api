c868f1b38ad92e645f6cc44730c46082c26618a6
Elmer Mendoza <emendoza@geo.gob.bo>
2014-06-13 19:19:41 -0400

    Modificación del script de deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+NPM_PATH=/home/emendoza/.nvm/v0.10.29
+
+PATH=$NPM_PATH/bin:$PATH
+export PATH NPM_PATH
+
+cd /var/www/geoelectoral-api
 git pull origin master
 npm install
 NOVE_ENV=production forever stop bin/www
