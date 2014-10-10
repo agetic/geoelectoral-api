df281f6e5d4acdb463843f5c5543091fa7d410af
Elmer Mendoza <emendoza@adsib.gob.bo>
2014-10-10 15:31:17 -0400

    Ajustes a la URL del WFS

--- controllers/proxy.js
+++ controllers/proxy.js
@@ -6,21 +6,21 @@ var http = require('http');
  * GET /api/v1/proxy?id_tipo_dpa=2
  */
 var dpa = function(req, res) {
-
+  console.log('rq.query', req.query);
   var cql_filter = 'id_dpa_antecesor={idDpa}+AND+id_tipo_dpa={idTipoDpa}';
   cql_filter = cql_filter.replace(/{idDpa}/g, req.query.idDpa);
   cql_filter = cql_filter.replace(/{idTipoDpa}/g, req.query.idTipoDpa);
 
   var generarOpcion = function(idTipoDpa, cql_filter) {
-    var path = '/geoserver/geoelectoral/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geoelectoral:{geoelectoralCapa}&maxFeatures=50&outputFormat=json&cql_filter=' + cql_filter;
+    var path = '/geoserver/geoelectoral/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geoelectoral:{geoelectoralCapa}&maxFeatures=5000&outputFormat=json&cql_filter=' + cql_filter;
     if (idTipoDpa == 2) {
-      path = path.replace(/{geoelectoralCapa}/g, 'geoelectoral-departamento');
-    } else if (idTipoDpa == 3 || idTipoDpa == 5) {
       path = path.replace(/{geoelectoralCapa}/g, 'geoelectoral-provincia');
+    } else if (idTipoDpa == 3 || idTipoDpa == 5) {
+      path = path.replace(/{geoelectoralCapa}/g, 'geoelectoral-municipio');
     } else if (idTipoDpa == 4) {
       path = path.replace(/{geoelectoralCapa}/g, 'geoelectoral-municipio');
     } else {
-      path = path.replace(/{geoelectoralCapa}/g, 'geoelectoral-dpa');
+      path = path.replace(/{geoelectoralCapa}/g, 'geoelectoral-departamento');
     }
     return {
       host: 'geo.gob.bo',
@@ -30,7 +30,7 @@ var dpa = function(req, res) {
     };
   };
 
-  var options = generarOpcion(req.query.idTipoDpa, cql_filter);
+  var options = generarOpcion(req.query.idTipoDpaActual, cql_filter);
 
   var req = http.request(options, function(result) {
     var body = '';
@@ -40,7 +40,11 @@ var dpa = function(req, res) {
     });
     result.on('end', function() {
       res.set('content-type', 'application/json; charset=UTF-8');
-      res.json(JSON.parse(body));
+      if (body.indexOf('<?xml') !== -1) {
+        res.json({error: "Error en el servidor de mapas"});
+      } else {
+        res.json(JSON.parse(body));
+      }
     });
   });
 
