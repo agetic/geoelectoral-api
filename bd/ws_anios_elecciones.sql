4c3fd1bdf4ff4216ba8d679c3b0dfc395c3fc04f
Marcelo Alejo <malejo@geo.gob.bo>
2015-04-23 16:21:37 -0400

    Actualizado script de anios disponibles, para mostrar elecciones subnacionales

--- bd/ws_anios_elecciones.sql
+++ bd/ws_anios_elecciones.sql
@@ -23,7 +23,7 @@ BEGIN
                                   GROUP BY id_eleccion, id_tipo_dpa
                                   ORDER BY c DESC) r
     ON e.id_eleccion=r.id_eleccion
-    WHERE e.id_tipo_eleccion IN (1, 2)
+    WHERE e.id_tipo_eleccion IN (1, 2, 6, 7)
     ORDER BY e.ano, e.id_eleccion, r.id_tipo_dpa;'
   ) USING 1;
 
