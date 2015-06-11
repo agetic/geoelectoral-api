5d2962b96a882ee35fca89ee6c2c5b3d2464f4a5
Marcelo Alejo <malejo@geo.gob.bo>
2015-06-11 18:49:30 -0400

    Correcion de filtro para consulta a geoserver

--- controllers/proxy.js
+++ controllers/proxy.js
@@ -20,10 +20,10 @@ var dpa = function(req, res) {
     path = path.replace(/{namespace}/g, config.app.geoserver.namespace);
     if(path.indexOf('id_tipo_dpa=6')>=0){
       path = path.replace(/{geoelectoralCapa}/g, 'geoelectoral-recinto');
-      path = path.replace('id_tipo_dpa=6','(id_tipo_dpa='+idTipoDpa+'+OR+id_tipo_dpa=6)+AND+fecha_creacion_corte<'+fecha+'+AND+'+fecha+'<fecha_supresion_corte');
+      path = path.replace('id_tipo_dpa=6','(id_tipo_dpa='+idTipoDpa+'+OR+id_tipo_dpa=6)+AND+fecha_creacion_corte<='+fecha+'+AND+'+fecha+'<=fecha_supresion_corte');
     } else if(path.indexOf('id_tipo_dpa=7')>=0){
       path = path.replace(/{geoelectoralCapa}/g, 'geoelectoral-recinto');
-      path = path.replace('id_tipo_dpa=7','id_tipo_dpa=6+AND+fecha_creacion_corte<'+fecha+'+AND+'+fecha+'<fecha_supresion_corte');
+      path = path.replace('id_tipo_dpa=7','id_tipo_dpa=6+AND+fecha_creacion_corte<='+fecha+'+AND+'+fecha+'<=fecha_supresion_corte');
     } else if (idTipoDpa == 2) {
       path = path.replace(/{geoelectoralCapa}/g, 'geoelectoral-provincia'+lzs);
     } else if (idTipoDpa == 3 || idTipoDpa == 5) {
