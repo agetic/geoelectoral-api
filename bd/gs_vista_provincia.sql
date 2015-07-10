4d090cc1007d08e760b336afe02c34ec839e531e
Marcelo Alejo <malejo@geo.gob.bo>
2015-07-09 20:36:27 -0400

    Correción de la vista provincias

--- bd/gs_vista_provincia.sql
+++ bd/gs_vista_provincia.sql
@@ -6,7 +6,7 @@ SELECT
   dpa.id_tipo_dpa,
   dpa.nombre,
   dpa.id_dpa_superior,
-  ST_Simplify(dpa.the_geom, 0.0185)) as the_geom,
+  ST_Simplify(dpa.the_geom, 0.0185) as the_geom,
   dpa.extent,
   dpa.area_km2,
   dpa.id_dpa_madre,
